8f38c75e3e353a925e51f3ad3ea3177744480717
juan francisco defranza <jfd087@gmail.com>
2021-03-08 18:12:56 -0300

    intro a la programacion

--- object-class.js
+++ object-class.js
@@ -3,3 +3,40 @@ var misdatos = {
     edad: 32,
     comidasprefes:["hamburguesa", "pizza"]
 }
+
+
+
+console.log(misdatos.nombre)
+
+
+class Persona{
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+    nombre = ""
+    edad = 0 
+}
+
+var yo = new Persona("Juan")
+console.log(yo.nombre)
+
+class Articulo{
+    constructor(a, b){
+        this.titulo = a;
+        this.texto = b; 
+    }
+    cambiarTitulo(nuevoTitulo){
+        this.titulo = nuevoTitulo
+    }
+    titulo = ""
+    texto = ""
+}
+
+var articuloPrimero = new Articulo("titulo a", "hola, como estas?");
+var articuloSegundo = new Articulo("titulo b", "hola, como estas b?");
+
+console.log(articuloPrimero);
+console.log(articuloSegundo);
+
+articuloPrimero.cambiarTitulo("nuevo titulo del articulo primero");
+console.log(articuloPrimero);
