4b33d2b93f0b46024019bd70d9bb1768015b3f29
juan francisco defranza <jfd087@gmail.com>
2021-03-15 16:42:03 -0300

    intro a la programacion

---
+++ sentence.js
@@ -0,0 +1,13 @@
+var nombre = "Juan";
+var numero = 45;
+
+var yo = {
+    nombre: "Juancho",
+    edad: 33,
+}
+
+if(nombre != "Ramiro"){
+    console.log("es Ramiro")
+}else{
+    console.log("no es ramiro")
+}
