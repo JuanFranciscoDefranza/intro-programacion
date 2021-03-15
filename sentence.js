2218b115578d9045bdceebf899d88d57bc4286d5
juan francisco defranza <jfd087@gmail.com>
2021-03-15 17:51:39 -0300

    intro a la programacion

--- sentence.js
+++ sentence.js
@@ -11,3 +11,29 @@ if(nombre != "Ramiro"){
 }else{
     console.log("no es ramiro")
 }
+
+var listadenumeros = [1, 8, 9, 10];
+
+for(var posicion = 0; posicion < listadenumeros.length; posicion++){
+    console.log(listadenumeros[posicion])
+}
+console.log("el final")
+
+var posicion = 0;
+
+while(posicion < listadenumeros.length){
+    console.log(listadenumeros[posicion]);
+    posicion++;
+}
+console.log("se termino")
+
+switch(nombre){
+    case "Juan":
+        console.log("es juan");
+    break;
+    case "Ramiro":
+        console.log("es ramiro");
+    break;
+    default:
+        console.log("no es ninguno");
+}
